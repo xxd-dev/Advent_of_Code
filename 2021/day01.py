ffc5e804c1e167d4b491480e261e53d4fb4da4d0
xxd-dev <93254605+xxd-dev@users.noreply.github.com>
2021-12-01 12:59:06 +0100

    .

--- 2021/day01.py
+++ 2021/day01.py
@@ -2,6 +2,6 @@ from aoc_util import as_int_list
 
 
 def solve():
-    depth = as_int_list('day01.txt')
+    depth = as_int_list('inputs/day01.txt')
     return len([1 for i in range(1, len(depth)) if depth[i] > depth[i - 1]]), \
            len([1 for i in range(3, len(depth)) if depth[i] > depth[i - 3]])
