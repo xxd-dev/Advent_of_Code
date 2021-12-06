478eaa922165c52b629abad80046dd6515d990b7
xxd-dev <93254605+xxd-dev@users.noreply.github.com>
2021-12-06 07:13:16 +0100

    .

--- 2021/day06.py
+++ 2021/day06.py
@@ -1,18 +1,18 @@
 def solve():
-    n = [int(x) for x in open('inputs/day06.txt').read().split(',')]
+    rabbits = [int(n) for n in open('inputs/day06.txt').read().split(',')]
     table = dict()
-    return sum([rabbits_rec(rabbit, 80, table) for rabbit in n]), \
-           sum([rabbits_rec(rabbit, 256, table) for rabbit in n])
+    return sum([population(rabbit, 80, table) for rabbit in rabbits]), \
+           sum([population(rabbit, 256, table) for rabbit in rabbits])
 
 
-def rabbits_rec(rabbit, amount, table):
+def population(rabbit, amount, table):
     if amount == 0:
         return 1
     if (rabbit, amount) in table:
         return table[(rabbit, amount)]
     if rabbit == 0:
-        value = rabbits_rec(6, amount - 1, table) + rabbits_rec(8, amount - 1, table)
+        value = population(6, amount - 1, table) + population(8, amount - 1, table)
     else:
-        value = rabbits_rec(rabbit - 1, amount - 1, table)
+        value = population(rabbit - 1, amount - 1, table)
     table[(rabbit, amount)] = value
     return value
