e7a33b1f9fef0002aba5491dd1aefea1eb7e534f
xxd-dev <93254605+xxd-dev@users.noreply.github.com>
2021-12-10 08:48:55 +0100

    .

--- 2021/day10.py
+++ 2021/day10.py
@@ -1,25 +1,22 @@
+import re
+
+
 def solve():
+    lines = [strip(line) for line in open('inputs/day10.txt').read().split('\n')]
     illegal_table = {')': 3, ']': 57, '}': 1197, '>': 25137}
-    lines = [valid(line) for line in open('inputs/day10.txt').read().split('\n')]
-    invalid_score = sum(illegal_table[line[1]] for line in lines if not line[0])
-    valid_scores = sorted([points(line[1]) for line in lines if line[0]])
-    return invalid_score, valid_scores[len(valid_scores)//2]
+    a = sorted([points(x[1]) for x in lines if x[0]])
+    return sum(illegal_table[line[1]] for line in lines if not line[0]), a[len(a)//2]
 
 
-def valid(line):
-    stack = []
-    for char in line:
-        if len(stack) > 0 and (stack[-1], char) in [('(', ')'), ('[', ']'), ('{', '}'), ('<', '>')]:
-            stack.pop(-1)
-        elif char in ')}>]':
-            return False, char
-        else:
-            stack.append(char)
-    return True, stack
+def strip(line):
+    while len(re.findall('\\(\\)|\\[\\]|\\{\\}|<>', line)) > 0:
+        line = re.sub('\\(\\)|\\[\\]|\\{\\}|<>', '', line)
+    cut = min([line.index(c) if c in line else len(line)+1 for c in ')]}>'])
+    return (False, line[cut]) if cut != len(line)+1 else (True, line[:cut])
 
 
 def points(array):
     score = 0
     for char in array[::-1]:
-        score = (5 * score) + '.([{<'.index(char)
+        score = (5 * score) + ' ([{<'.index(char)
     return score
