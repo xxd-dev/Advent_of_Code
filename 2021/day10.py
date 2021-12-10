9fbbfbb0d2bc1a92bd44c25ccef53551876c5412
xxd-dev <93254605+xxd-dev@users.noreply.github.com>
2021-12-10 07:15:46 +0100

    .

--- 2021/day10.py
+++ 2021/day10.py
@@ -1,12 +1,12 @@
 def solve():
     illegal_table = {')': 3, ']': 57, '}': 1197, '>': 25137}
-    lines = [corrupted(line) for line in open('inputs/day10.txt').read().split('\n')]
+    lines = [valid(line) for line in open('inputs/day10.txt').read().split('\n')]
     invalid_score = sum(illegal_table[line[1]] for line in lines if not line[0])
     valid_scores = sorted([points(line[1]) for line in lines if line[0]])
     return invalid_score, valid_scores[len(valid_scores)//2]
 
 
-def corrupted(line):
+def valid(line):
     stack = []
     for char in line:
         if len(stack) > 0 and (stack[-1], char) in [('(', ')'), ('[', ']'), ('{', '}'), ('<', '>')]:
