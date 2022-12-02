c34bbf52023758dc64da1a836b4ec7ad25ce04ee
xxd-dev <93254605+xxd-dev@users.noreply.github.com>
2022-12-02 18:09:46 +0100

    .

---
+++ 2022/day01.py
@@ -0,0 +1,7 @@
+from aoc_util import as_int_list
+
+
+def solve():
+    file = open('inputs/day01.txt').read()
+    calories = sorted([sum([int(n) for n in elv.split('\n')]) for elv in file.split('\n\n')])[::-1]
+    return calories[0], sum(calories[0:3])
