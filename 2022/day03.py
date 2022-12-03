2fbc1ab38701233041ae343ae94adf7dc495b2e5
xxd-dev <93254605+xxd-dev@users.noreply.github.com>
2022-12-03 13:05:29 +0100

    .

---
+++ 2022/day03.py
@@ -0,0 +1,12 @@
+from aoc_util import as_2d_list
+
+
+def solve():
+    file = open('inputs/day03.txt').read().split('\n')
+    alphabet = '_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    task1 = sum([alphabet.index((set(rucksack[:len(rucksack) // 2]) & set(rucksack[len(rucksack) // 2:])).pop())
+                 for rucksack in file])
+    task2 = sum([alphabet.index((set(file[i]) & set(file[i+1]) & set(file[i+2])).pop())
+                 for i in range(0, len(file), 3)])
+    return task1, task2
+
